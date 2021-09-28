33e67744f35f8ead3c9f89c255220f58e715f156
Sergey <nalivaykosergeyreal@gmail.com>
2021-09-28 20:25:33 +0300

    Added tag v2.0

--- NetMonitoring/src/Model.py
+++ NetMonitoring/src/Model.py
@@ -1,14 +1,18 @@
 from mininet.topo import Topo
+from mininet.link import TCLink
 
+'''Файл моделируемой топологии'''
 
 class OwnTopology(Topo):
 
     def __init__(self, **opts):
         super(OwnTopology, self).__init__(**opts)
-        host1 = self.addHost('h%s' % 1, cpu=.5 / 3, ip="10.0.0.1/8")
-        host2 = self.addHost('h%s' % 2, cpu=.5 / 3, ip="10.0.0.2/8")
-        host3 = self.addHost('h%s' % 3, cpu=.5 / 3, ip="10.0.0.3/8")
+        
+        '''Создание узлов сети'''
+        host1 = self.addHost('h%s' % 1)
+        host2 = self.addHost('h%s' % 2)
         switch1 = self.addSwitch('s%s' % 1)
-        self.addLink(host1, switch1, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True)
-        self.addLink(host2, switch1, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True)
-        self.addLink(host3, switch1, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True)
+        ''''Соединение узлов с заданными характеристиками'''
+        self.addLink(host1, switch1, cls=TCLink, bw=100, delay='25ms', loss=1, max_queue_size=50)
+        self.addLink(host2, switch1, cls=TCLink, bw=100, delay='25ms', loss=1, max_queue_size=50)
+        
--- NetMonitoring/src/main.py
+++ NetMonitoring/src/main.py
@@ -1,25 +1,16 @@
 from mininet.net import Mininet
+from mininet.cli import CLI
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 from Model import OwnTopology
-from Monitor import Monitor
-
 
 
 def simulation():
     topology = OwnTopology()
     net = Mininet(topo=topology, host=CPULimitedHost, link=TCLink)
-    monitor = Monitor()
-
     net.start()
-    monitor.monitoring_qlen(interval_sec=0.1)
-
-    h1, h2 = net.get('h1', 'h2')
-    monitor.iperf_output(h1, h2,  "-t 20")
-
-    monitor.stop_monitoring()
+    CLI(net)
     net.stop()
-
 
 
 if __name__ == '__main__':
